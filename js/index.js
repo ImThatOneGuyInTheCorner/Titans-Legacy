b1f7ff0116edbcf4ddc5278effb7d78ad6b73c16
Ryan Duran <112416371+Ryanduran001@users.noreply.github.com>
2024-04-22 10:41:37 -0700

    oberlay pt2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
-function overlayOn() {
-    document.getElementById("how-to-play-overlay").style.display = "flex";
-}
-function overlayOff() {
-    document.getElementById("how-to-play-overlay").style.display = "flex";
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Add listener to create start button
+    if ( document.getElementById('how-to-play-overlay') !== null) {
+        let howToPlay = document.getElementById('howToPlay');
+        howToPlay.addEventListener('click', overlayShuffle)
+    }
+});
+function overlayShuffle() {
+    document.getElementById("how-to-play-overlay").classList.toggle("hidden");
 }
