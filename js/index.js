66ae2cf1e8cca490b18db6ebafb07d14d81a4376
Finn Poduska <finnpoduska@gmail.com>
2024-04-24 08:40:36 -0700

    How to play

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
     // Add listener to create start button
     if ( document.getElementById('how-to-play-overlay') !== null) {
         let howToPlay = document.getElementById('howToPlay');
         howToPlay.addEventListener('click', overlayShuffle)
     }
+    if ( document.getElementById('closeHowToPlayButton') !== null) {
+        let howToPlay = document.getElementById('closeHowToPlayButton');
+        howToPlay.addEventListener('click', overlayShuffle)
+    }
 });
 function overlayShuffle() {
     document.getElementById("how-to-play-overlay").classList.toggle("hidden");
+    document.getElementById("how-to-play-overlay").classList.toggle("flex");
 }
