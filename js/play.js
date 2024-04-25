86a0630c26e0fff204fe0e633d09e994250d5593
Ryan Duran <112416371+Ryanduran001@users.noreply.github.com>
2024-04-25 08:45:20 -0700

    fixed resource names

--- js/play.js
+++ js/play.js
@@ -18,9 +18,9 @@ class Player {
         this.resources = new Map;
         this.resources.set("wood",0)
         this.resources.set("mushroom",0)
-        this.resources.set("deer",0)
-        this.resources.set("ore",0)
-        this.resources.set("clay",0)
+        this.resources.set("orange",0)
+        this.resources.set("gem",0)
+        this.resources.set("stone",0)
         this.titan;
         this.name;
     }
@@ -187,15 +187,15 @@ let buildings = {
     "outpost": {
         "texture": "assetoutpost",
         "costs": {
-            "clay": 1,
+            "stone": 1,
             "wood": 1,
-            "deer": 1
+            "orange": 1
         }
     },
     "road": {
         "texture": "assetroad",
         "costs": {
-            "clay": 1,
+            "stone": 1,
             "wood": 1
         }
     }
